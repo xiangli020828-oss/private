7923bec064f64851a67b0ad9dfa29e9975544b0f
xiangli020828-oss <xiangli020828@gmail.com>
2026-01-13 15:30:10 +0100

    Update Textures.java

--- Textures.java
+++ Textures.java
@@ -12,5 +12,16 @@ public class Textures {
     public static final TextureRegion FLOWERS = SpriteSheet.BASIC_TILES.at(2, 5);
 
     public static final TextureRegion CHEST = SpriteSheet.BASIC_TILES.at(5, 5);
+
+    //增加不同状态的Fence
+    public static final TextureRegion FENCE_Up = SpriteSheet.BASIC_TILES.at(4,2);
+    public static final TextureRegion FENCE_UpperLeft_Corner = SpriteSheet.BASIC_TILES.at(4,1);
+    public static final TextureRegion FENCE_UpperRight_Corner = SpriteSheet.BASIC_TILES.at(4,3);
+    public static final TextureRegion FENCE_Left = SpriteSheet.BASIC_TILES.at(5,1);
+    public static final TextureRegion FENCE_Right = SpriteSheet.BASIC_TILES.at(5,3);
+    public static final TextureRegion FENCE_Low = SpriteSheet.BASIC_TILES.at(6,2);
+    public static final TextureRegion FENCE_LowerLeft_Corner = SpriteSheet.BASIC_TILES.at(6,1);
+    public static final TextureRegion FENCE_LowerRight_Corner = SpriteSheet.BASIC_TILES.at(6,3);
+
     
 }
